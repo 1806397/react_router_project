5cc28dc5dce92cb87e406338d87dad422df2ddf9
Piyush Singh <1806397@kiit.ac.in>
2022-12-20 12:57:40 +0530

    3rd commit:Added nested routes

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,10 +1,14 @@
-import { useParams } from "react-router-dom";
+import { Route, useParams } from "react-router-dom";
+import Comments from "../components/comments/Comments";
 const QuoteDetail = () => {
   const params = useParams();
   return (
     <>
       <h1>All Quotes Page</h1>
       <p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
     </>
   );
 };
